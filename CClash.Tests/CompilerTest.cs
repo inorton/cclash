03a139df92e735da70642217f0f5c4a253be7560
Ian Norton <inorton@gmail.com>
2013-10-17 18:13:59 +0100

    add compiler tests

---
+++ CClash.Tests/CompilerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using CClash;
+
+namespace CClash.Tests
+{
+    [TestFixture]
+    public class CompilerTest
+    {
+
+        [Test]
+        [TestCase( "/c","test-sources\\exists.c" )]
+        public void ParseSupportedArgs(params string[] argv)
+        {
+            var c = new Compiler();
+            Assert.IsTrue( c.ProcessArguments(argv) );
+        }
+
+        [Test]
+        [TestCase( "/c", "test-sources\\doesnotexist.c" )]
+        public void ParseUnSupportedArgs(params string[] argv)
+        {
+            var c = new Compiler();
+            Assert.IsFalse(c.ProcessArguments(argv));
+        }
+    }
+}
--- CClash/Compiler.cs
+++ CClash/Compiler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,29 @@ namespace CClash
     {
         public string[] CommandLine { get; set; }
 
-        public string SourceFile { get; set; }
-        public string ObjectFile { get; set; }
+        public string SourceFile { 
+            get {
+                return srcs.FirstOrDefault();
+            }
+        }
+        public string ObjectTarget { get; set; }
+        public bool ObjectTargetIsDirectory { get; set; }
+
         public string PdbFile { get; set; }
 
         public bool Linking { get; set; }
         public bool PrecompiledHeaders { get; set; }
-        public bool SingleSource { get; set; }
+        public bool GeneratePdb { get; set; }
+
+        List<string> srcs = new List<string>();
+
+        public bool SingleSource
+        {
+            get
+            {
+                return srcs.Count == 1;
+            }
+        }
 
         public bool IsSupported
         {
@@ -26,14 +43,95 @@ namespace CClash
                     !PrecompiledHeaders &&
                     SingleSource &&
                     !String.IsNullOrWhiteSpace(SourceFile) &&
-                    !String.IsNullOrWhiteSpace(ObjectFile) 
+                    !String.IsNullOrWhiteSpace(ObjectTarget) &&
+                    File.Exists(SourceFile)
                     );
             }
         }
 
-        public void ProcessArguments(string[] args)
+        string getOption( string arg )
         {
-            CommandLine = args;
+            if ( arg.StartsWith("-") || arg.StartsWith("/") ){
+                return arg.Substring(1);
+            }
+            return arg;
+        }
+
+        public bool ProcessArguments(string[] args)
+        {
+            try
+            {
+                CommandLine = args;
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (getOption(args[i]))
+                    {
+                        case "Yu":
+                            PrecompiledHeaders = true;
+                            return false;
+
+                        case "link":
+                            Linking = true;
+                            return false;
+                        case "Zi":
+                            GeneratePdb = true;
+                            break;
+                        case "Fd":
+                            PdbFile = args[++i];
+                            break;
+                        
+                        case "Fo":
+                            var tgt = args[++i];
+                            ObjectTarget = tgt;
+                            if (Directory.Exists(tgt))
+                            {
+                                ObjectTargetIsDirectory = true;
+                            }
+                            break;
+
+                        case "Tp":
+                        case "Tc":
+                        case "c":
+                            var srcfile = args[++i];
+                            if (File.Exists(srcfile))
+                            {
+                                srcs.Add(srcfile);
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+                if (SingleSource){
+                    if (ObjectTargetIsDirectory)
+                    {
+                        ObjectTarget = Path.Combine( ObjectTarget, Path.GetFileName(SourceFile) );
+                    }
+
+                    if (ObjectTarget == null)
+                    {
+                        ObjectTarget = Path.Combine(Path.GetDirectoryName(SourceFile), Path.GetFileNameWithoutExtension(SourceFile)) + ".obj";
+                    }
+                     
+                    if (GeneratePdb && string.IsNullOrEmpty(PdbFile))
+                    {
+                        PdbFile = Path.Combine(Path.GetDirectoryName(ObjectTarget), Path.GetFileNameWithoutExtension(ObjectTarget));
+                    }
+                }
+            }
+            catch ( Exception e )
+            {
+                Console.Error.WriteLine(e);
+                return false;
+            }
+
+            return IsSupported;
         }
     }
 }
