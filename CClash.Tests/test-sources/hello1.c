1ca283e15938e6fa16385cfc71878e1a73be434b
Ian Norton <inorton@gmail.com>
2014-05-22 22:12:05 +0100

    test source files

---
+++ CClash.Tests/test-sources/hello1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#include "hello.h"
+
+#include "hello2.h"
+
+#include "exists.c"
+
+#include "another.h"
+
+int main( int argc, char** argv )
+{
+#ifdef FOO
+	printf(FOO);
+#endif
+
+	return HELLO_TEST;
+}
