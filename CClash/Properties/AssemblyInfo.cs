2d6e1f92e66de1756564f263aa7d5b52b078ed0d
Ian Norton <inorton@gmail.com>
2016-04-03 13:38:01 +0100

    bump assembly versions

--- CClash/Properties/AssemblyInfo.cs
+++ CClash/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("cclash")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CClash Compiler Cache")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ian Norton")]
 [assembly: AssemblyProduct("cclash")]
 [assembly: AssemblyCopyright("Copyright Ian Norton ©2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyVersion("0.3.16.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
