8209c3375642af138cb8da27f19273558df45561
Ian Norton <inorton@gmail.com>
2016-05-11 09:24:44 +0100

    add test c++ source

---
+++ CClash.Tests/test-sources/hello.cc
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+   vector<string> SS;
+
+   SS.push_back("The number is 10");
+   SS.push_back("The number is 20");
+   SS.push_back("The number is 30");
+
+   cout << "Loop by index:" << endl;
+
+   int ii;
+   for(ii=0; ii < SS.size(); ii++)
+   {
+      cout << SS[ii] << endl;
+   }
+
+}
